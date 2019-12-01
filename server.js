c5bc4df797161acc0c0d2c1846622bde8342bc03
Jashwanthi V <54614090+Jashwanthi928@users.noreply.github.com>
2019-12-01 11:18:43 +0530

    Updated app.listen

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use((req,res,next)=>{
 })
 
 app.set('view engine','hbs')
-app.listen(port,() => console.log('server started on port 5000.....'))
+app.listen(port,() => console.log('Listening to port 5000.....'))
 
 app.use('/maxprofit',maxprofit)
 app.use('/',wholesalecontrollers)
