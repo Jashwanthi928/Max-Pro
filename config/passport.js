3fe1ccac819db232c322d38c80c5974d07f64230
Jashwanthi V <54614090+Jashwanthi928@users.noreply.github.com>
2019-12-01 11:14:09 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const User = mongoose.model('User');
 
+
 module.exports = function (passport) {
   passport.use(new LocalStrategy({
     usernameField: 'emailid'
